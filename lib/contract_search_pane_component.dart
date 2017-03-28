67872fae838ce5162d4f6feca2ae251cd1204527
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-28 16:18:22 +0300

    Dimitriy fix

--- lib/contract_search_pane_component.dart
+++ lib/contract_search_pane_component.dart
@@ -1,13 +1,1 @@
-import 'package:angular2/core.dart';
-import 'src/panes/abstract_pane.dart';
-
-@Component(selector: 'contract-search-pane')
-@View(templateUrl: 'contract_search_pane_component.html')
-class ContractSearchPaneComponent implements AbstractPane {
-  String id = 'contract-search-pane';
-  Type type = ContractSearchPaneComponent;
-  String iconClass = 'fa fa-file';
-  String paneClass = '';
-
-  ContractSearchPaneComponent();
-}
+export 'src/panes/contract_search/contract_search_pane_component.dart';
