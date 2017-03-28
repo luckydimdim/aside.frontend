2ab09023fa70e8869a828c8478382c6f45e7843c
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-28 16:06:50 +0300

    Dimitriy fix

--- lib/src/panes/contract_search/contract_search_pane_component.dart
+++ lib/src/panes/contract_search/contract_search_pane_component.dart
@@ -1,5 +1,5 @@
 import 'package:angular2/core.dart';
-import 'package:aside/src/panes/abstract_pane.dart';
+import '../../panes/abstract_pane.dart';
 
 @Component(selector: 'contract-search-pane')
 @View(templateUrl: 'contract_search_pane_component.html')
